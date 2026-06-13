008bc6db2430ac8c937f2aff7d9fd9e670bee0ca
Tsvetozar B. <contact@chechu.dev>
2026-06-14 01:27:52 +0200

    feat: update Storage interface to include CertChain in SaveToken and LoadToken

--- internal/core/ports/storage.go
+++ internal/core/ports/storage.go
@@ -1,6 +1,8 @@
 package ports
 
+import "github.com/licenselatte/latte-go/internal/core/domain"
+
 type Storage interface {
-	SaveToken(token string) error
-	LoadToken() (string, error)
+	SaveToken(token string, chain *domain.CertChain) error
+	LoadToken() (string, *domain.CertChain, error)
 }
