6ad578778d799ee95c875546a5338a4df79ea979
Tsvetozar B. <contact@chechu.dev>
2026-06-14 01:27:48 +0200

    feat: update MemoryStorage to handle CertChain in SaveToken and LoadToken

--- internal/infra/storage/memory.go
+++ internal/infra/storage/memory.go
@@ -1,20 +1,25 @@
 package storage
 
-import "github.com/licenselatte/latte-go/internal/core/ports"
+import (
+	"github.com/licenselatte/latte-go/internal/core/domain"
+	"github.com/licenselatte/latte-go/internal/core/ports"
+)
 
 type MemoryStorage struct {
 	token string
+	chain *domain.CertChain
 }
 
 func NewMemoryStorage() ports.Storage {
 	return &MemoryStorage{}
 }
 
-func (s *MemoryStorage) SaveToken(token string) error {
+func (s *MemoryStorage) SaveToken(token string, chain *domain.CertChain) error {
 	s.token = token
+	s.chain = chain
 	return nil
 }
 
-func (s *MemoryStorage) LoadToken() (string, error) {
-	return s.token, nil
+func (s *MemoryStorage) LoadToken() (string, *domain.CertChain, error) {
+	return s.token, s.chain, nil
 }
